946bb865acdc795e2d8ed151e75bf7b66d1e76f6
0u812 <0u812@users.noreply.github.com>
2017-02-22 17:00:51 -0800

    move vcard dialog to own module

--- src/notebook/components/notebook.js
+++ src/notebook/components/notebook.js
@@ -1,12 +1,10 @@
 /* eslint-disable no-return-assign */
 /* @flow */
-const {dialog, BrowserWindow, app} = require('electron').remote;
 import React from 'react';
 import { DragDropContext as dragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { connect } from 'react-redux';
-import {checkVCardExists, writeDummyVCard} from '../vcard'
-const path = require('path');
+import {checkVCardExists, writeDummyVCard, vcardChecks} from '../vcard'
 
 import { List as ImmutableList, Map as ImmutableMap } from 'immutable';
 
@@ -70,43 +68,7 @@ const mapStateToProps = (state: Object) => ({
 });
 
 export function preExecuteCellChecks(store: Object, id: String, cell: Object): Boolean {
-  if (!checkVCardExists()) {
-    const response = dialog.showMessageBox({
-      type: 'question',
-      title: 'VCard Not Found',
-      buttons: ['Yes', 'No'],
-      // TODO: Upgrade Electron
-      // checkboxLabel: 'Do not show again',
-      // checkboxChecked: false,
-      message: 'Would you like to fill in your personal info now?',
-      detail:  'Could not find you personal info. '+
-      'Would you like to fill in your personal info now? '+
-      'This will automatically add your name to any COMBINE archives you create.'
-    }//,
-    // callback
-    // (response, checkboxChecked) => {
-    // (response) => {
-    //   alert(response);
-    //   if (response === 0) {
-    //     alert('yes');
-    //   }
-    // }}
-    );
-    if (response === 0) {
-      // Create the VCard and do not execute the cell
-      var vcard_dialog = new BrowserWindow({width: 600, height: 400, useContentSize: true, title: 'VCard Info', show: false});
-      const vcard_page = path.join(app.getAppPath(), 'static', 'vcard.html');
-      vcard_dialog.loadURL('file://'+vcard_page);
-      vcard_dialog.once('ready-to-show', () => {
-        vcard_dialog.show();
-      });
-      return false;
-    } else {
-      // Create a dummy VCard and execute the cell
-      writeDummyVCard();
-    }
-  }
-  return true;
+  return vcardChecks(store, id, cell);
 }
 
 export function executeCellInNotebook(store: Object, id: String, cell: Object): void {
--- src/notebook/vcard.js
+++ src/notebook/vcard.js
@@ -1,3 +1,4 @@
+const {dialog, BrowserWindow, app} = require('electron').remote;
 import { writeFileSync } from 'fs';
 const path = require('path');
 const os = require('os');
@@ -24,4 +25,44 @@ export function writeDummyVCard(): void {
     email: username.sync()+'@dankmemes.com',
     organization: ''
   }));
+}
+
+export function vcardChecks(store: Object, id: String, cell: Object): Boolean {
+  if (!checkVCardExists()) {
+    const response = dialog.showMessageBox({
+      type: 'question',
+      title: 'VCard Not Found',
+      buttons: ['Yes', 'No'],
+      // TODO: Upgrade Electron
+      // checkboxLabel: 'Do not show again',
+      // checkboxChecked: false,
+      message: 'Would you like to fill in your personal info now?',
+      detail:  'Could not find you personal info. '+
+      'Would you like to fill in your personal info now? '+
+      'This will automatically add your name to any COMBINE archives you create.'
+    }//,
+    // callback
+    // (response, checkboxChecked) => {
+    // (response) => {
+    //   alert(response);
+    //   if (response === 0) {
+    //     alert('yes');
+    //   }
+    // }}
+    );
+    if (response === 0) {
+      // Create the VCard and do not execute the cell
+      var vcard_dialog = new BrowserWindow({width: 600, height: 400, useContentSize: true, title: 'VCard Info', show: false});
+      const vcard_page = path.join(app.getAppPath(), 'static', 'vcard.html');
+      vcard_dialog.loadURL('file://'+vcard_page);
+      vcard_dialog.once('ready-to-show', () => {
+        vcard_dialog.show();
+      });
+      return false;
+    } else {
+      // Create a dummy VCard and execute the cell
+      writeDummyVCard();
+    }
+  }
+  return true;
 }
